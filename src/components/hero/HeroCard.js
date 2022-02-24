8c7bee4be4ab6c3eeeacf1f760557ac113b0487f
Daniel Ramirez Castano <dm.ramirez@globant.com>
2022-02-24 14:51:35 -0500

    Fixed images route

--- src/components/hero/HeroCard.js
+++ src/components/hero/HeroCard.js
@@ -11,7 +11,8 @@ export const HeroCard = ({
   characters,
 }) => {
 
-  const imagePath = `assets/${id}.jpg`;
+  // const imagePath = `assets/${id}.jpg`;
+  const imagePath = `../../../public/assets/${id}.jpg`;
 
   return (
     <div className="col">
