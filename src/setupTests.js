99f67586d9682ac5f5bdb7fee6db08db6cdc4c45
Daniel Ramirez Castano <dm.ramirez@globant.com>
2022-02-25 17:30:41 -0500

    prepared app for tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,23 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { createSerializer } from 'enzyme-to-json'
+
+Enzyme.configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+
+
+
+// IMPORT ALL THIS
+// npm i --save-dev enzyme
+// npm install --save-dev enzyme-to-json
+// npm install --save-dev @wojtekmaj/enzyme-adapter-react-17
+
+// import Enzyme from 'enzyme';
+// import { createSerializer } from 'enzyme-to-json'
+// import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+// Enzyme.configure({ adapter: new Adapter() });
+// expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
